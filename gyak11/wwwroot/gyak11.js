d78735570ee78510c10026a52781f74170ea0345
M29VLR <hojana1570@gmail.com>
2023-11-27 15:00:23 +0100

    gyak11

---
+++ gyak11/wwwroot/gyak11.js
@@ -0,0 +1,16 @@
+﻿console.log('%c Oh my heavens! ', 'background: #222; color: #bada55');
+
+
+window.onload = function letöltés() {
+    var viccek;
+    fetch('/jokes.json')
+        .then(response => response.json())
+        .then(data => letöltésBefejeződött(data)
+        );
+
+    function letöltésBefejeződött(d) {
+        console.log("Sikeres letöltés")
+        HTMLBodyElement.writeline(d)
+        viccek = d;
+    }
+}
